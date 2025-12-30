eab77b2f41f945a0b338d903165913d16b24f1d1
hencci <hencci20@gmail.com>
2025-12-30 16:58:59 +0100

    data: add names list

---
+++ src/data/names.js
@@ -0,0 +1,11 @@
+export const names = [
+  "Daniel Samuel",
+  "Elpah-Chris",
+  "@Phildeoner",
+  "@Kadunapikin",
+  "@Chef.Tsang",
+  "@Akoke",
+  "@Austin",
+  "Hope Jonah",
+  "Henry Moses",
+];
